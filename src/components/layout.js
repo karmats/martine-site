0d83a78aa747cb5d605f2745358f09e8acc5391d
Mats Roshauw <karmats@gmail.com>
2019-07-21 10:45:54 +0200

    Removed most of gatsby references, minor changes

--- src/components/layout.js
+++ src/components/layout.js
@@ -14,7 +14,7 @@ import "./layout.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
+    query HeaderQuery {
       site {
         siteMetadata {
           title
@@ -37,7 +37,7 @@ const Layout = ({ children }) => {
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
-          {` `}
+          {' '}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
       </div>
